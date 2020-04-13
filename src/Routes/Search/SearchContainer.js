744b218957d9d283c06cc1104dfdcdfde57ff5ba
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-04-13 22:59:51 +0900

    Set presenters of Home & TV and added Search functionality

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -12,14 +12,23 @@ export default class extends React.Component{
 
     componentDidMount(){
         this.handleSubmit();
-    }
+    };
 
-    handleSubmit = () => {
+    handleSubmit = event => {
+        if(event){ 
+            event.preventDefault()};
         const { searchTerm } = this.state;
         if(searchTerm !==""){
             this.searchByTerm();
         }
+    };
 
+    updateTerm = event => {
+        const { target : {value} } = event;
+        console.log(value);
+        this.setState({
+            searchTerm: value
+        });
     }
 
     searchByTerm = async() => {
@@ -50,6 +59,7 @@ export default class extends React.Component{
             loading={loading} 
             error={error}
             handleSubmit={this.handleSubmit}
+            updateTerm={this.updateTerm}
         />
         );
     }
