b673b25d87b12ce889889d22de1f716ee4624a5d
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-04-15 23:06:36 +0900

    Added jsconfig.json

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
 import { moviesApi, tvApi } from "../../api";
-import Message from "../../Components/Message";
 export default class extends React.Component{
     state = {
         movieResults: null,
